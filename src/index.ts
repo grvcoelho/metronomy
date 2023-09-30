b973c17f8d1a0b5c3be441cd8f0b3ff5ff3c089d
Guilherme Coelho <guilhermervcoelho@gmail.com>
2023-09-30 17:33:44 -0300

    Draw a line

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-console.log(123)
+;(async function main() {
+  const paper = document.querySelector('#paper') as HTMLCanvasElement
+  const pen = paper.getContext('2d')
+  const startTime = Date.now()
+
+  paper.width = paper.clientWidth
+  paper.height = paper.clientHeight
+
+  draw()
+
+  function draw() {
+    if (!pen || !paper) return
+
+    const currentTime = Date.now()
+    const ellapsedTime = currentTime - startTime
+
+    const start = {
+      x: paper.width * 0.1,
+      y: paper.height * 0.9,
+    }
+
+    const end = {
+      x: paper.width * 0.9,
+      y: paper.height * 0.9,
+    }
+
+    const center = {
+      x: paper.width * 0.5,
+      y: paper.height * 0.9,
+    }
+
+    const lineLength = end.x - start.x
+
+    pen.lineCap = 'round'
+    pen.strokeStyle = 'white'
+    pen.lineWidth = 4
+
+    pen.beginPath()
+    pen.moveTo(start.x, start.y)
+    pen.lineTo(end.x, end.y)
+    pen.stroke()
+  }
+})()
