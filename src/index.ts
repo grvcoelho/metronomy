ebd721cb1b9cf2b4a7bec13ab48de26b4f2a82ed
Guilherme Coelho <guilhermervcoelho@gmail.com>
2023-09-30 17:52:35 -0300

    Initialize arcs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { createStore } from '@guifromjs/store'
 
+type Arc = {
+  velocity: number
+  nextImpactTime: number
+}
+
 function getStore() {
-  const numberOfArcs = 2
+  const numberOfArcs = 15
   const maxCycles = Math.round(numberOfArcs * 1.25)
   const duration = 60 * 0.2
   const startTime = Date.now()
-
-  const arcs = Array.from({ length: numberOfArcs })
+  const arcs = new Array<Arc>(numberOfArcs).fill({} as Arc)
 
   const store = createStore({
     settings: {
@@ -29,7 +33,31 @@ function getStore() {
   paper.width = paper.clientWidth
   paper.height = paper.clientHeight
 
+  store.subscribe(console.info)
+
+  store.setState((draft) => {
+    draft.arcs.forEach((arc, index) => {
+      arc.velocity = calculateVelocityForArc(index)
+      arc.nextImpactTime = calculateNextImpactTime(
+        draft.settings.startTime,
+        arc.velocity
+      )
+    })
+  })
+
   draw()
+
+  function calculateVelocityForArc(index) {
+    const { settings } = store.getState()
+    const numberOfCycles = settings.maxCycles - index
+    const distancePerCycle = Math.PI
+
+    return (numberOfCycles * distancePerCycle) / settings.duration
+  }
+
+  function calculateNextImpactTime(currentImpactTime, velocity) {
+    return currentImpactTime + (Math.PI * velocity) / 1000
+  }
 
   function draw() {
     if (!pen || !paper) return
